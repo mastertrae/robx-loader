f334b154babf174e62d41207ad7d07497d96a500
mastertrae <mastertrae526@gmail.com>
2026-09-02 18:28:46 +0800

    Update script URL in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/dN9Vq27W"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/mastertrae/8413c71c21f9907b1b9dbf5a605663b8/raw/a4d99c8f593fa5cd72dae98e48a2c7f7fd810db6/robx"))()
